891e97396db4fcaefbb8ae152613cbd9a5cc9253
Drew English <drew@on-line-support.com>
2018-12-28 16:16:43 -0800

    Update. Check for correct indexing and for efficiency.

--- NeuralNet.cpp
+++ NeuralNet.cpp
@@ -140,13 +140,12 @@ void Network::fit(vector<double> const &input, vector<double> const &target)
 
   //making a copy of hidden neuron values for the case of the hidden activation being relu
 	//as both the unrelu and relu values of the hidden neurons are needed
+  vector<double> h;
 	if (this->hiddenLayers > 0 && this->actFunHidden == relu)
 	{
-		vector<double> h;
 		for (int i = 0; i < this->hiddenLayers * this->numHidden; i++)
 			h.push_back(this->hiddenNeurons[i]);
 	}
-
   if(this->actFunOut == relu)
   {
     for(int i = 0; i < this->outputs; i++)
@@ -176,5 +175,25 @@ void Network::fit(vector<double> const &input, vector<double> const &target)
 		delta[i + this->hiddenLayers * this->numHidden] = dOut(o[i]) * (o[i] - target[i]);
 	}
 
+  //calculate deltas for hidden layers if any
+	for (int i = this->hiddenLayers; i > 0; i--)
+	{
+		//finds first weight and delta in next layer
+		vector<double>::iterator w = this->weights.begin() + ((this->inputs + 1) * this->numHidden) + ((this->numHidden + 1) * this->numHidden * (i - 1)) + 1;
+		vector<double>::iterator d = delta.begin() + (this->numHidden * i);
 
+		for (int j = 0; j < this->numHidden; j++)
+		{
+			delta[this->numHidden * (i - 1) + j] = 0;
+
+			for (int k = 0; k < (i == this->hiddenLayers ? this->outputs : this->numHidden); k++)
+			{
+				delta[this->numHidden * (i - 1) + j] += w[(this->numHidden + 1) * k + j] * d[k];
+			}
+
+			//doing this calculation now to keep hidden all deltas in the same format
+			//(makes it easier when changing weights)
+			delta[this->numHidden * (i - 1) + j] *= dHidden(h[j]);
+		}
+	}
 }
